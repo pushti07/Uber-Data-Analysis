dfe3f7aecefc0802c74c5ddc10344ee6a8f7fec5
Pushti Maheshwari <144027532+pushti07@users.noreply.github.com>
2025-03-27 22:46:17 +0530

    Update UberDataAnalysis.py

--- UberDataAnalysis.py
+++ UberDataAnalysis.py
@@ -2,3 +2,25 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+df = pd.read_csv('UberDataset.csv')
+df
+
+df.info()
+
+# Data Preprocessing
+df['PURPOSE'].fillna('NOT', inplace = True)
+df
+
+df['START_DATE'] = pd.to_datetime(df['START_DATE'], errors = 'coerce')
+
+df['END_DATE'] = pd.to_datetime(df['END_DATE'], errors = 'coerce')
+
+df.info()
+
+from datetime import datetime
+
+df['date'] = pd.DatetimeIndex(df['START_DATE']).date
+df['time'] = pd.DatetimeIndex(df['START_DATE']).hour
+
+df.head()
